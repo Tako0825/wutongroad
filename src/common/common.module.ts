da3a5cadce60a913b63cb2f07fe6a104026b3b24
Tako0825 <2060364922@qq.com>
2023-09-19 10:37:15 +0800

    通用模块整理至CommonModule

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { CommonService } from './common.service';
+import { WechatApiModule } from './wechat-api/wechat-api.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Global()
 @Module({
+  imports: [WechatApiModule, PrismaModule],
   providers: [CommonService],
-  exports: [CommonService]
+  exports: [CommonService],
 })
 export class CommonModule {}
