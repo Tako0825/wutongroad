e1a03d69fd93314510f5629c3fcee3c371f06392
Tako0825 <2060364922@qq.com>
2023-09-21 21:54:02 +0800

    客户端在线列表-监控用户的在线和离线行为

--- src/notice/notice.gateway.ts
+++ src/notice/notice.gateway.ts
@@ -1,18 +1,47 @@
-import { MessageBody, OnGatewayConnection, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { WebSocket } from 'ws';
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer, WsException } from '@nestjs/websockets';
+import { User } from '@prisma/client';
+import { IncomingMessage } from 'http';
+import { CommonService } from 'src/common/common.service';
+import { PrismaModel } from 'src/common/enum/PrismaModel';
+import { PrismaService } from 'src/common/prisma/prisma.service';
+import { Server, WebSocket } from 'ws';
 
 @WebSocketGateway(3001, { path: "/notice" })
-export class NoticeGateway implements OnGatewayConnection {
-  handleConnection(socket: WebSocket, dto: any) {
-    console.log("连接成功");
+export class NoticeGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server
+  // 客户端在线列表
+  onlineList: object = new Object()
+
+  constructor(
+    private prisma:PrismaService,
+    private commonService:CommonService
+  ) {}
+
+  handleConnection(client: WebSocket, data: IncomingMessage) {
   }
 
-  @SubscribeMessage('chat')
-  handelChat(@MessageBody() data:any, socket: WebSocket) {
-    console.log(data);
-    return {
-      event: "chat",
-      data
+  handleDisconnect(client: WebSocket) {
+    // 遍历客户端在线列表, 将当前离线用户从列表中移除
+    for(let key in this.onlineList) {
+      if(this.onlineList[key] === client) {
+        delete this.onlineList[key]
+        console.log("断开连接-1");
+      }
+    }
+  }
+
+  @SubscribeMessage("user-connect")
+  async handleUserConnect(@MessageBody() user_id:string, @ConnectedSocket() client: any) {
+    // 尝试判断用户是否存在, 若存在则将其添加至在线列表
+    try {
+      const user:User = await this.commonService.getEntityByUuid(PrismaModel.user, user_id)
+      if(!this.onlineList[user_id]) {
+        this.onlineList[user_id] = client
+        console.log(`在线用户+1————${user.nickname}(${user.uuid})`);
+      }
+    } catch(httpException) {
+      throw new WsException(httpException)
     }
   }
 }
